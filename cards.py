8a341bcdfe78dbdb0604a4b42d4af08303cd3979
Asmee-tech <asmee.khimani@gmail.com>
2025-10-18 10:53:43 +0200

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,41 @@
+import pygame
+pygame.init()
+width=600
+height=400
+screen=pygame.display.set_mode((width,height))
+#loading images
+img1=pygame.image.load("diwali_bg1.jpg")
+img2=pygame.image.load("diwali_bg2.jpg")
+img3=pygame.image.load("diwali_bg3.jpeg")
+img4=pygame.image.load("diwali_bg4.jpeg")
+# resizing img
+img1=pygame.transform.scale(img1,(width,height))
+img2=pygame.transform.scale(img2,(width,height))
+img3=pygame.transform.scale(img3,(width,height))
+img4=pygame.transform.scale(img4,(width,height))
+#loading font
+font=pygame.font.SysFont("lobster",45)
+
+
+
+
+
+
+
+
+
+run=True
+while run:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            run=False
+    screen.blit(img1,(0,0))
+    text1=font.render("Keep calm and eat sweets, it’s Diwali!",True,"white")
+    screen.blit(text1,(30,100))
+    #screen.blit(img2,(0,0))
+    #screen.blit(img3,(0,0))
+    #screen.blit(img4,(0,0))
+    
+    
+    pygame.display.update()
+pygame.quit()
