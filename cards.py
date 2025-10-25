8009da237f24945a77ded124452828ad5a84f738
Asmee-tech <asmee.khimani@gmail.com>
2025-10-25 10:43:57 +0200

    Implement timed message and image display
    Added timed display of multiple messages and images.

--- cards.py
+++ cards.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 pygame.init()
 width=600
 height=400
@@ -17,13 +18,6 @@ img4=pygame.transform.scale(img4,(width,height))
 font=pygame.font.SysFont("lobster",45)
 
 
-
-
-
-
-
-
-
 run=True
 while run:
     for event in pygame.event.get():
@@ -32,9 +26,26 @@ while run:
     screen.blit(img1,(0,0))
     text1=font.render("Keep calm and eat sweets, it’s Diwali!",True,"white")
     screen.blit(text1,(30,100))
-    #screen.blit(img2,(0,0))
-    #screen.blit(img3,(0,0))
-    #screen.blit(img4,(0,0))
+    pygame.display.update()
+    time.sleep(2)
+    screen.blit(img2,(0,0))
+    text2=font.render("When in doubt, add more lights!",True,"white")
+    screen.blit(text2,(40,100))
+    pygame.display.update() 
+    time.sleep(2)
+    screen.blit(img3,(0,0))
+    text3=font.render("Sugar,shine,and shenanigans!",True,"white")
+    screen.blit(text3,(40,100))
+    pygame.display.update()
+    time.sleep(2)
+    screen.blit(img4,(0,0))
+    text4=font.render("Glow goals achieved",True,"white")
+    screen.blit(text4,(40,100))
+    pygame.display.update()
+    time.sleep(2)
+    
+    pygame.display.update()
+pygame.quit()
     
     
     pygame.display.update()
